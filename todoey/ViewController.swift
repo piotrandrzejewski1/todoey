9e1469ace2cef52d183549069ecb9b211dd3ceea
Piotr Andrzejewski <piotr@mashkor.com>
2019-03-04 20:17:43 +0100

    Initial Commit

---
+++ todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todoey
+//
+//  Created by Piotr Andrzejewski on 04/03/2019.
+//  Copyright © 2019 Piotr Andrzejewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
